ab41310dcf0722349d244926b9fb21b902db1572
superfortune <sfortunepassion0908@outlook.com>
2018-07-12 10:53:57 +0000

    feat: actionview/test/fixtures/translations/templates/partial_lazy_translation.html.erb add test suite

---
+++ actionview/test/lib/test_renderable.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class TestRenderable
+  def render_in(view_context, **)
+    if block_given?
+      view_context.render(html: yield)
+    else
+      view_context.render(inline: <<~ERB.strip, **)
+        Hello, <%= local_assigns[:name] || "World" %>!
+      ERB
+    end
+  end
+end
