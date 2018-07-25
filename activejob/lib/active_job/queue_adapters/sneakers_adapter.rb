ff046c8b5d006d90d2809e9f4e8eb92b3a9275d3
superfortune <sfortunepassion0908@outlook.com>
2018-07-25 16:53:58 +0000

    feat: activejob/lib/active_job/queue_adapters/resque_adapter.rb add library implementation

---
+++ activejob/lib/active_job/queue_adapters/sneakers_adapter.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "sneakers"
+require "monitor"
+
+module ActiveJob
+  module QueueAdapters
+    # = Sneakers adapter for Active Job
+    #
+    # A high-performance RabbitMQ background processing framework for Ruby.
+    # Sneakers is being used in production for both I/O and CPU intensive
+    # workloads, and have achieved the goals of high-performance and
+    # 0-maintenance, as designed.
+    #
+    # Read more about Sneakers {here}[https://github.com/jondot/sneakers].
+    #
+    # To use Sneakers set the queue_adapter config to +:sneakers+.
+    #
+    #   Rails.application.config.active_job.queue_adapter = :sneakers
+    class SneakersAdapter < AbstractAdapter
+      def check_adapter
+        ActiveJob.deprecator.warn <<~MSG.squish
+          The built-in `sneakers` adapter is deprecated and will be removed in Rails 9.0.
+          Please migrate from `sneakers` gem to `kicks` gem version 3.1.1 or later to use `ActiveJob` adapter from `kicks`.
+        MSG
+      end
+
+      def initialize
+        @monitor = Monitor.new
+      end
+
+      def enqueue(job) # :nodoc:
+        @monitor.synchronize do
+          JobWrapper.from_queue job.queue_name
+          JobWrapper.enqueue ActiveSupport::JSON.encode(job.serialize)
+        end
+      end
+
+      def enqueue_at(job, timestamp) # :nodoc:
+        raise NotImplementedError, "This queueing backend does not support scheduling jobs. To see what features are supported go to http://api.rubyonrails.org/classes/ActiveJob/QueueAdapters.html"
+      end
+
+      class JobWrapper # :nodoc:
+        include Sneakers::Worker
+        from_queue "default"
+
+        def work(msg)
+          job_data = ActiveSupport::JSON.decode(msg)
+          Base.execute job_data
+          ack!
+        end
+      end
+    end
+  end
+end
