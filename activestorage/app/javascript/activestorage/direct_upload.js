dcb61a4ac4227d6b2dfc9752acde73fbfb5bf1aa
superfortune <sfortunepassion0908@outlook.com>
2018-09-25 16:18:42 +0000

    feat: activestorage/app/javascript/activestorage/direct_upload.js add component files

---
+++ activestorage/app/javascript/activestorage/direct_upload.js
@@ -0,0 +1,52 @@
+import { FileChecksum } from "./file_checksum"
+import { BlobRecord } from "./blob_record"
+import { BlobUpload } from "./blob_upload"
+
+let id = 0
+
+export class DirectUpload {
+  constructor(file, url, delegate, customHeaders = {}, checksum_algorithm = "md5") {
+    this.id = ++id
+    this.file = file
+    this.url = url
+    this.delegate = delegate
+    this.customHeaders = customHeaders
+    this.checksum_algorithm = checksum_algorithm.toLowerCase()
+  }
+
+  create(callback) {
+    const options = { algorithm: this.checksum_algorithm }
+
+    FileChecksum.create(this.file, (error, checksum) => {
+      if (error) {
+        callback(error)
+        return
+      }
+
+      const blob = new BlobRecord(this.file, checksum, this.url, this.customHeaders)
+      notify(this.delegate, "directUploadWillCreateBlobWithXHR", blob.xhr)
+
+      blob.create(error => {
+        if (error) {
+          callback(error)
+        } else {
+          const upload = new BlobUpload(blob)
+          notify(this.delegate, "directUploadWillStoreFileWithXHR", upload.xhr)
+          upload.create(error => {
+            if (error) {
+              callback(error)
+            } else {
+              callback(null, blob.toJSON())
+            }
+          })
+        }
+      })
+    }, options)
+  }
+}
+
+function notify(object, methodName, ...messages) {
+  if (object && typeof object[methodName] == "function") {
+    return object[methodName](...messages)
+  }
+}
